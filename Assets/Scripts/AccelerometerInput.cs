cbc7031c83ca13d01051cecdf65f2959d937ba17
Claudio Sampaio <claudio.sampaio@trdreams.com>
2016-05-21 18:40:54 -0400

    New features: - Ape jump - Camera follow - Player death.

--- Assets/Scripts/AccelerometerInput.cs
+++ Assets/Scripts/AccelerometerInput.cs
@@ -3,10 +3,6 @@ using System.Collections;
 
 public class AccelerometerInput : MonoBehaviour 
 {
-	/*void Update () 
-	{
-		transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
-	}*/
 
 	public float characterGameSpeed = 10.0F;
 	public float screenLimitsAxisX = 2.5F;
@@ -16,28 +12,20 @@ public class AccelerometerInput : MonoBehaviour
 	}
 
 	void Update() {
-		Vector3 dir = Vector3.zero;
-		//Debug.Log ("Input Acceleration x: " + Input.acceleration.x);
-		//Debug.Log ("Input Acceleration z: " + Input.acceleration.z);
-		dir.x = Input.acceleration.x;
-		/*dir.z = Input.acceleration.z;*/
-		if (dir.sqrMagnitude > 1)
-			dir.Normalize();
-
-		dir *= Time.deltaTime;
-		transform.Translate(dir.x * characterGameSpeed,0,0);
+        
+        Vector3 dir = Vector3.zero;        
+        dir.x = Input.acceleration.normalized.x;
+        dir *= Time.deltaTime * characterGameSpeed;
+		transform.Translate(dir);
 
 		//Debug.Log ("transform.localPosition.x: " + transform.localPosition.x);
-		if (transform.localPosition.x > screenLimitsAxisX) {
-			
-			transform.Translate(-(transform.localPosition.x-screenLimitsAxisX),0,0);
+		if (transform.localPosition.x > screenLimitsAxisX) {			
+			transform.Translate(-(transform.localPosition.x-screenLimitsAxisX), 0, 0);
 		}
 
 		if (transform.localPosition.x < -screenLimitsAxisX) {
 
-			transform.Translate(-transform.localPosition.x-screenLimitsAxisX,0,0) ;
+			transform.Translate(-transform.localPosition.x-screenLimitsAxisX, 0, 0) ;
 		}
-
-		
-	}
+    }
 }
