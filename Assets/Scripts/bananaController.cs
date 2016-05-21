fe357bd98460a5cd4e77119f574ea386bd1317e8
Claudio Sampaio <claudio.sampaio@trdreams.com>
2016-05-21 15:00:16 -0400

    Create player, banana, banana counter, brain counter prefabs. Create scene background brefab.

---
+++ Assets/Scripts/bananaController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bananaController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
