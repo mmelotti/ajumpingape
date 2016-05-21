bc94c265e0c62ffce4b41a4c9005e553a468b6be
brucostam <brucostam@gmail.com>
2016-05-21 14:04:21 -0300

    game menu buttons added. play button sprite added. scripts to handle score added. some settings changed to try a first Android build.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+	
+	private static ScoreManager _instance;
+
+	public int lastScore;
+	public int bestScore;
+
+	public static ScoreManager Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				GameObject fbm = new GameObject("ScoreManager");
+				fbm.AddComponent<ScoreManager>();
+			}
+
+			return _instance;
+		}
+	}
+
+
+	void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+		_instance = this;
+
+	}
+
+	//TODO: Save a local score and load on start or awake.
+	
+}
---
+++ Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateScore : MonoBehaviour {
+
+	public GameObject lastScoreButton;
+	public GameObject bestScoreButton;
+
+	// Use this for initialization
+	void Start () {
+
+		ScoreManager sm = ScoreManager.Instance;
+
+		lastScoreButton.GetComponentInChildren<Text>().text = sm.lastScore.ToString();
+		bestScoreButton.GetComponentInChildren<Text>().text = sm.bestScore.ToString();
+	}
+		
+
+}
