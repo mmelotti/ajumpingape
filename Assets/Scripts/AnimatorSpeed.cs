f5c8ac7aceefcc526ec127f5a96a44a05a8b90d6
brucostam <brucostam@gmail.com>
2016-05-22 05:01:28 -0300

    Changes to make the parallax background movement

---
+++ Assets/Scripts/AnimatorSpeed.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorSpeed : MonoBehaviour {
+
+	private Animator anim;
+	public float animatorSpeed = 1;
+
+	void Start () 
+	{
+		anim = gameObject.GetComponent<Animator> ();
+		anim.speed = animatorSpeed;
+	}
+		
+}
---
+++ Assets/Scripts/ParallaxScroller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScroller : MonoBehaviour {
+
+	public float scrollSpeedRatio;
+	public float tileSizeZ;
+
+	public GameObject camera;
+
+	private Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+		startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+		Debug.Log ("new position " + newPosition);
+		Vector3 aux = transform.localPosition;
+		aux.y = newPosition;
+		transform.localPosition = aux; //startingPosition + Vector3.forward * newPosition;*/
+
+		Vector3 aux = Vector3.zero;
+		aux.y = -((camera.transform.position.y*scrollSpeedRatio) % (startingPosition.y*2))+startingPosition.y;
+
+		if (aux.y < -startingPosition.y) {
+			aux.y = startingPosition.y;
+		}
+
+		transform.localPosition = aux;
+	}
+
+	/*void OnDisable()
+	{
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+	}*/
+}
